ff8d492cf03d7ff1932c27a3fdb752c570f8c426
Tristen <TristenS@protonmail.com>
2019-10-06 14:00:44 -0400

    Add error handling and dates.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
+import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -29,13 +30,15 @@ app.secret_key = "qwertyuiop"
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(5000))
+    body = db.Column(db.Text)
+    date = db.Column(db.DateTime)
     # This will be used when I implement users
     #owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, date):
         self.title = title
         self.body = body
+        self.date = date
         #self.owner = owner_id
 
 # Saving this class for when it is time to implement users
@@ -53,11 +56,37 @@ class Post(db.Model):
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    title_error = ""
+    body_error = ""
+    posts = Post.query.all()
+    if request.method == "POST":
+        post_title = request.form["post-title"]
+        post_body = request.form["new-post"]
 
-    return render_template("posts.html")
+        if post_title == "" or post_body == "":
+            if post_title == "":
+                title_error = "Must have a title."
+
+            if post_body == "":
+                body_error = "Wheres the post!?"
+
+            return render_template("posts.html", title="Build a Blog",
+                                   posts=posts, title_error=title_error, body_error=body_error, post_title=post_title, post_body=post_body)
+
+        new_post = Post(post_title, post_body, datetime.datetime.now())
+        db.session.add(new_post)
+        db.session.commit()
+    posts = Post.query.all()
+
+    return render_template("posts.html", title="Build a Blog", posts=posts,
+                           title_error=title_error, body_error=body_error)
 
 
-@app.route("add", methods=["POST"])
+@app.route("/add", methods=["POST", "GET"])
 def add_post():
 
     return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run()
